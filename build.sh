a53f952214ff4ee3c56397fdc0287a90fb3a455b
LaboDJ <2527836+labodj@users.noreply.github.com>
2023-03-02 12:34:35 +0100

    build.sh: fixes
    Set endline to LF (Unix)
    Fixes for dirs containing spaces
    Avoid >> for files

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-rm -rf ${SCRIPT_DIR}/build
-mkdir -p ${SCRIPT_DIR}/build
+rm -rf "${SCRIPT_DIR}/build"
+mkdir -p "${SCRIPT_DIR}/build"
 
-cp ${SCRIPT_DIR}/src/bootstrap.sh ${SCRIPT_DIR}/build/rpi-init.sh
+cp "${SCRIPT_DIR}/src/bootstrap.sh" "${SCRIPT_DIR}/build/rpi-init.sh"
 
-for module in ${SCRIPT_DIR}/src/scripts/*
-do
-    moduleDir=$(realpath --relative-to=${SCRIPT_DIR}/src $module)
+for module in "${SCRIPT_DIR}"/src/scripts/*; do
+    moduleDir=$(realpath --relative-to="${SCRIPT_DIR}/src" "${module}")
     echo -e "\n# {{$moduleDir}}" | tee -a "${SCRIPT_DIR}/build/rpi-init.sh" >/dev/null
-    cat $module >> ${SCRIPT_DIR}/build/rpi-init.sh
+    tee -a "${SCRIPT_DIR}/build/rpi-init.sh" <"${module}"
 done
 
-chmod a+x ${SCRIPT_DIR}/build/rpi-init.sh
+chmod a+x "${SCRIPT_DIR}/build/rpi-init.sh"
